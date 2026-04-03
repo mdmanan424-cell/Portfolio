3b78fa7d937f659f2f16bbf9b605a114036f96a8
mdmanan424-cell <mdmanan424@gmail.com>
2026-04-03 12:38:47 +0530

    Update personal information and project links

--- js/constants.js
+++ js/constants.js
@@ -1,34 +1,34 @@
 // Project constants with placeholders
 const APP_DATA = {
-    name: "Bharath D",
+    name: "Mohammed Mannan",
     role: "Full Stack Developer",
     introduction: "I’m a Full Stack Java Developer with a strong foundation in building scalable and user-centric web applications using Spring Boot, REST APIs, and modern frontend technologies. I recently graduated in Computer Science (VTU, 2025) and gained hands-on industry experience during my internship at Eunoia Labs, where I worked on real-world full stack workflows.",
-    resumeLink: "Bharath_D_Resume_3.pdf",
+    resumeLink: "Mannan_Resume.pdf",
     socialLinks: {
-        email: "bharathdhananjaya155@gmail.com",
-        linkedin: "https://linkedin.com/in/bharath-d-7a196a353",
-        github: "https://github.com/bharathdhananjaya155-sketch"
+        email: "mdmanan424@gmail.com",
+        linkedin: "http://linkedin.com/in/md-manan-j053",
+        github: "https://github.com/mdmanan424-cell"
     },
     projects: [
         {
             title: "Resume Builder",
             description: "The Resume Builder is a full-stack web application that enables users to create, customize, and manage professional resumes dynamically. I developed the backend using Spring Boot, where I designed RESTful APIs to handle various sections of resume data such as education, skills, and work experience. The application uses MySQL for persistent data storage and follows a structured layered architecture to ensure scalability and maintainability.",
             tech: ["SpringBoot", "Rest Api", "React", "MySql"],
-            link: "https://resume-builder-frontend-0y6j.onrender.com/",
+            link: "https://md-resume-builder.netlify.app/",
             image: "images/project1.png"
         },
         {
             title: "Background Remove",
             description: "The AI Background Removal Tool is a web application that allows users to remove image backgrounds instantly using artificial intelligence. I developed the backend using Spring Boot and integrated the Clipdrop API to process images efficiently and return high-quality results. The application supports image uploads, processes them securely, and provides real-time feedback to the user.",
             tech: ["SpringBoot", "Rest Api", "React", "MySql"],
-            link: "https://background-remove-frontend.onrender.com/",
+            link: "https://md-background-remove.netlify.app/",
             image: "images/project2.png"
         },
         {
             title: "Quiz Master",
             description: "The Quiz Application is a backend-driven system designed to manage quiz creation, question retrieval, and score evaluation efficiently. I developed RESTful APIs using Spring Boot, enabling functionalities such as adding quizzes, fetching questions, and calculating scores. The application follows a layered MVC architecture, ensuring clean code organization and scalability. MySQL was used to store quiz data and user scores.",
             tech: ["SpringBoot", "Rest Api", "Js", "MySql"],
-            link: "https://quiz-master-17.netlify.app/",
+            link: "https://md-quiz-master.netlify.app/",
             image: "images/project3.png"
         }
     ],
@@ -43,7 +43,7 @@ const APP_DATA = {
             role: "Java Full Stack Developer Intern",
             company: "Tap Academy",
             date: "July 2025 - January 2026",
-            certificateLink: "https://drive.google.com/file/d/1O-ezoAJdEvA1qp3UGjgLwvF6NpkNZYzs/view?usp=drive_link"
+            certificateLink: "https://drive.google.com/file/d/1_fHagYUKOCnnOz6oY4KTTpd7-rvws1hV/view?usp=drivesdk"
         }
     ],
     skills: [
